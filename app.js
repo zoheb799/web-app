4a10af56e0c6c0596aac22347730f674cb2dae4d
zoheb799 <96819670+zoheb799@users.noreply.github.com>
2022-05-29 22:12:09 +0530

    backend

---
+++ app.js
@@ -0,0 +1,118 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// var DB = mongoose.connect('mongodb+srv://zero:<zero>@cluster0.nu0uy.mongodb.net/zero?retryWrites=true&w=majority').then(() => {
+// console.log('connection to DB successful');
+// }).catch((err) => console.log('no connection to DB'));
+
+// mongodb+srv://<username>:<password>@cluster0.trgam.mongodb.net/?retryWrites=true&w=majority
+
+
+
+mongoose
+  .connect(
+    "mongodb+srv://zero:zero@cluster0.nu0uy.mongodb.net/zero?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to the database!!");
+  })
+  .catch((error) => {
+    console.log("error!!");
+  });
+
+
+// // }).catch((err) => console.log('no connection to DB'));
+// // ;
+// // connect to DB
+// // const DB = 'mongodb+srv://zero:<zero>@cluster0.nu0uy.mongodb.net/zero?retryWrites=true&w=majority';
+// // mongoose.connect(DB).then(() => {
+// console.log('connection to DB successful');
+
+// }).catch((err) => console.log('no connection to DB'));
+
+
+var product = require('./models/product');
+var WishList = require('./models/wishlist');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+var ingredients = [
+    {
+      "id":"983j",
+      "text":"chicken biryani",
+    },
+  
+    {
+      "id":"983j", 
+      "text":"veg biryani",
+    },
+  
+    {
+      "id":"983j",
+      "text":"mutton biryani",
+    },
+    {
+      "id":"983j",
+      "text":"prawns biryani",
+    },
+    {
+      "id":"983j",
+      "text":"water bottle or cool drinks",
+    },
+  ];
+  app.get('/Menu', function(request, response) {
+    response.send(ingredients);
+  }); 
+
+app.post('/product', function(request, response) {
+    var Product = new product();
+    product.title = request.body.title;
+    product.price = request.body.price;
+    Product.save(function(err, SavedProduct) {
+        if (err) {
+            response.status(500).send({error:"could not save the product"});
+        } else {
+            response.send(SavedProduct);
+        }
+    });
+   
+});
+
+
+
+//tested and successfully connected to database
+// app.get("/",(req,res)=>{
+//   res.send("testing");
+// })
+
+app.post('/product', function(request, response) {
+  var product = new Product();
+    if (!product || product.text === "") {
+      response.status(500).send({error: "enter your details"})
+    } else {
+      product.push(product);
+      response.status(200).send(product);
+    }
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("swag-shop API is running on port 3000!");
+
+}); 
---
+++ posts.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const router = require('express/lib/response');
+
+// const router = express.router();
+// router.get('/', (request, respond) => {
+//     res.send("we are running on posts");
+// });
+
+// module.exports = router;
