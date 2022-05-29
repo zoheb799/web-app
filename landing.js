e8d9dab5376c15768a0eb96329c4ccba80989dca
zoheb799 <96819670+zoheb799@users.noreply.github.com>
2022-05-29 22:10:18 +0530

    front end

---
+++ landing.js
@@ -0,0 +1,35 @@
+
+    function myOnClickFn() {
+     var num = document.forms["myForm"]["numb"].value;
+     var un = document.forms["myForm"]["uname"].value;
+     var pno = document.forms["myForm"]["phnumb"].value;
+     if(num == "" && un == "" && pno == "") {
+      alert("please enter your details.");
+     } else {
+       
+   
+   window.location.href = "main.html";
+
+     }
+    }
+
+
+
+
+    function myOnClick() {
+      // var num = document.forms["myForm"]["numb"].value;
+      // var un = document.forms["myForm"]["uname"].value;
+      // var pno = document.forms["myForm"]["phnumb"].value;
+      // if(num == "" && un == "" && pno == "") {
+      //  alert("please enter your details.");
+      // } else {
+        
+    
+    document.location.href = "Menu.html";
+ 
+      }
+     
+    
+
+
+    // https://github.com/athoutam1/Restaurant-Reservation
